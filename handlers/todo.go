3733562666cc82603d02634be293628935fd2014
Gokhan Kocer <gkkocer@gmail.com>
2022-11-11 15:14:24 -0800

    Post Request Added

--- handlers/todo.go
+++ handlers/todo.go
@@ -1,7 +1,16 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gokhankocer/TODO-API/database"
+	"github.com/gokhankocer/TODO-API/models"
+)
 
 func AddTodo(c *gin.Context) {
-	return
+	var todo models.Todo
+	c.BindJSON(&todo)
+	database.DB.Create(&todo)
+	c.JSON(http.StatusOK, &todo)
 }
